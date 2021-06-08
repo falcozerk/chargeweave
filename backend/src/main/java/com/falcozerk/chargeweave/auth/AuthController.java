6f635a43352c9f857c800928ab969b56472e6879
Timothy Falconer <teefal@gmail.com>
2021-06-08 15:56:14 -0400

    hello endpoint and lombox change

--- backend/src/main/java/com/falcozerk/chargeweave/auth/AuthController.java
+++ backend/src/main/java/com/falcozerk/chargeweave/auth/AuthController.java
@@ -14,10 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -42,6 +39,11 @@ public class AuthController {
 
     @Autowired
     JwtTokenProvider tokenProvider;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hi, welcome to Chargeweave.";
+    }
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
