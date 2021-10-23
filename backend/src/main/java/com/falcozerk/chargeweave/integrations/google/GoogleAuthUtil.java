e931cf930b289094b3190a76798b141585fd7acb
Timothy Falconer <teefal@gmail.com>
2021-10-22 21:46:01 -0400

    moved app endpoints to app

--- backend/src/main/java/com/falcozerk/chargeweave/integrations/google/GoogleAuthUtil.java
+++ backend/src/main/java/com/falcozerk/chargeweave/integrations/google/GoogleAuthUtil.java
@@ -6,7 +6,7 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.MemoryDataStoreFactory;
 import com.google.api.services.sheets.v4.SheetsScopes;
 
@@ -20,13 +20,13 @@ import java.util.List;
 public class GoogleAuthUtil {
     public static Credential authorize() throws IOException, GeneralSecurityException {
         InputStream in = GoogleAuthUtil.class.getResourceAsStream("/google-sheets-credentials.json");
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JacksonFactory.getDefaultInstance(), new InputStreamReader(in));
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(GsonFactory.getDefaultInstance(), new InputStreamReader(in));
 
         List<String> scopes = Arrays.asList(SheetsScopes.SPREADSHEETS);
         GoogleAuthorizationCodeFlow flow =
                 new GoogleAuthorizationCodeFlow.Builder(
                         GoogleNetHttpTransport.newTrustedTransport(),
-                        JacksonFactory.getDefaultInstance(),
+                        GsonFactory.getDefaultInstance(),
                         clientSecrets,
                         scopes).setDataStoreFactory(new MemoryDataStoreFactory())
                 .setAccessType("offline").build();
