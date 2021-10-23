ccf522e6a3b00ca69948b935cef79c4b684dd0eb
Timothy Falconer <teefal@gmail.com>
2021-10-22 21:44:42 -0400

    moved app endpoints to app

--- backend/src/main/java/com/falcozerk/chargeweave/beans/app/AppController.java
+++ backend/src/main/java/com/falcozerk/chargeweave/beans/app/AppController.java
@@ -3,6 +3,7 @@ package com.falcozerk.chargeweave.beans.app;
 import com.falcozerk.chargeweave.beans.common.CwController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/app")
 public class AppController extends CwController {
-    private static final Logger logger = LoggerFactory.getLogger(AppController.class);
+    static final Logger logger = LoggerFactory.getLogger(AppController.class);
 
-    private AppService appService;
+    @Autowired
+    AppService appService;
 
     @GetMapping("/importData")
     public String importData() {
